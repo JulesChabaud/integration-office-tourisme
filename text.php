c9baf189293f8dfd234e315fcac439bc456ddd08
laurinehrd <laurine.hrd@hotmail.fr>
2020-06-18 16:45:47 +0200

    initialisation header

--- text.php
+++ text.php
@@ -1,15 +1,34 @@
 <?php include "header.php" ?>
-<link rel="stylesheet" href="text.css">
+<link rel="stylesheet" href="assets/css/text.css">
 
 <!--SECTION NAVBAR -->
-  <header>
-    <h1 class="text-uppercase">Aéroport Paris-Beauvais</h1>
+
+    <h1 class="text-uppercase text-h1">Aeroport Paris-Beauvais</h1>
+
+
+<!-- SECTION BANNER -->
+  <header class="text-header">
+    <div class="text-imgBanner">
+      <img class="text-imgBgBanner" src="assets/images/text/page_paris_beauvais.jpg" alt="image avion"/>
+      <div class="container-fluid text-containerBanner">
+        <div class="row">
+          <div class="col-lg-6">
+            <p>Situé sur la commune de Tillé, à 2km au Nord de Beauvais et à environ 70km de Paris.</p>
+            <p>Avec plus de 60 destinations</p>
+            <input class="text-buttonDecouvrir text-uppercase py-2" type="button" value="Découvrir">
+          </div>
+          <div class="col-lg-6 text-iconSocialMedia">
+            <!--icone-->
+            <a href="#"><img src="assets/images/text/icon-google.png" alt="icone google"/></a>
+            <a href="#"><img src="assets/images/text/icon-twitter.png" alt="icone twitter"/></a>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
 
   </header>
-
-
-
 
 
 
