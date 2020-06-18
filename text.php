a7a5e48aa0ecd25cbf28109a157c524f6e2a8a7d
laurinehrd <laurine.hrd@hotmail.fr>
2020-06-18 11:10:52 +0200

    initialisation projet

---
+++ text.php
@@ -0,0 +1,24 @@
+<?php include "header.php" ?>
+<link rel="stylesheet" href="text.css">
+
+<!--SECTION NAVBAR -->
+  <header>
+    <h1 class="text-uppercase">Aéroport Paris-Beauvais</h1>
+
+
+  </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include "footer.php" ?>
