5adf7469b71437091ae2ee78580bc610ff60874d
laurinehrd <laurine.hrd@hotmail.fr>
2020-06-19 12:01:04 +0200

    init banner

--- text.php
+++ text.php
@@ -18,17 +18,57 @@
             <input class="text-buttonDecouvrir text-uppercase py-2" type="button" value="Découvrir">
           </div>
           <div class="col-lg-6 text-iconSocialMedia">
-            <!--icone-->
-            <a href="#"><img src="assets/images/text/icon-google.png" alt="icone google"/></a>
+            <a href="#"><img src="assets/images/text/icon-facebook.png" alt="icone facebook"/></a>
+            <a href="#"><img class="text-iconGoogle" src="assets/images/text/icon-google-+.png" alt="icone google"/></a>
             <a href="#"><img src="assets/images/text/icon-twitter.png" alt="icone twitter"/></a>
           </div>
         </div>
       </div>
-    </div> 
+    </div>
 
-
-
+    <div class="text-breadcrumbs">
+      <a href="#"><h3 class="text-uppercase text-fontOrange"><span class="font-weight-bold">A</span>ccueil ></h3></a>
+      <a href="#"><h3 class="text-uppercase text-fontOrange"><span class="font-weight-bold">A</span>éroport de Paris Beauvais ></h3></a>
+      <a href="#"><h3 class="text-uppercase text-fontBlack"><span class="font-weight-bold">P</span>aris Beauvais</h3></a>
+    </div>
   </header>
+
+<!-- SECTION SERVICE -->
+  <section>
+    <div class="text-titleServices">
+      <h2 class="text-uppercase"><span class="text-fontPurple">Les services <br></span><span class="text-fontWhite">de l'aéroport</span></h2>
+      <img class="text-iconRestau" src="assets/images/text/text-iconRestauServices.png" alt="icone restaurant"/>
+
+    </div>
+    <div class="container-fluid text-imgServices">
+      <div class="row">
+        <div class="col-lg-6 p-0">
+          <img src="assets/images/text/services-sandwich.jpg" alt="image comptoir sandwich"/>
+        </div>
+        <div class="col-lg-6 p-0">
+          <img src="assets/images/text/services-vin.jpg" alt="image magasin vin"/>
+        </div>
+      </div>
+    </div>
+
+    <div class="text-encadreText">
+      <p>Un aéroport est une mini ville et ce n'est pas l'Aéroport de Paris-Beauvais qui vous montrera l'inverse. Chacun de ses deux terminaux vous proposera des boutiques, des restaurants, des snacks de quoi acheter des souvenirs ou combler votre faim.</p>
+    </div>
+
+    <div class="text-infoWifiAtm">
+      <div class="text-wifi">
+        <!--icone wifi-->
+        <p>Vous n'avez pas envie de vous balader en attendant votre avion, profitez alors de ses nombreux sièges ainsi que du Wifi gratuit et illimité !</p>
+      </div>
+      <div class="text-atm">
+        <!--icon atm-->
+        <p>Votre destination est l'Angleterre, la Hongrie ou la Pologne, vous n'avez pas eu le temps de changer votre monnaie, pas de panique le distributeur automatique au terminal 2 convertira votre monnaie dans la devise étrangère souhaitée.</p>
+      </div>
+    </div>
+
+
+
+  </section>
 
 
 
